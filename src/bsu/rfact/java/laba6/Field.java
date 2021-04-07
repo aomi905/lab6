24226750dc96b9839d3755b4b6f66b5ef0caf9fe
aomi905 <hanna.s905@gmail.com>
2021-04-08 01:06:14 +0300

    Field

--- src/bsu/rfact/java/laba6/Field.java
+++ src/bsu/rfact/java/laba6/Field.java
@@ -1,8 +1,51 @@
 package bsu.rfact.java.laba6;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Field extends JPanel {
-    public void canMove(BouncingBall bouncingBall) {
+    private boolean paused;
+    private List<BouncingBall> balls = new ArrayList<>(10);
+
+    private Timer repaintTimer = new Timer(10, new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            repaint();
+        }
+    });
+
+    public Field(){
+        setBackground(Color.WHITE);
+        repaintTimer.start();
+    }
+
+    public void paintComponent(Graphics graphics){
+        super.paintComponent(graphics);
+        Graphics2D canvas = (Graphics2D) graphics;
+        for (BouncingBall ball : balls){
+            ball.paint(canvas);
+        }
+    }
+    public void addBall(){
+        balls.add(new BouncingBall(this));
+    }
+
+    public synchronized void pause(){
+        paused = true;
+    }
+
+    public synchronized void resume(){
+        paused = false;
+        notifyAll();
+    }
+
+    public synchronized void canMove(BouncingBall bouncingBall) throws InterruptedException{
+        if (paused){
+            wait();
+        }
     }
 }
--- src/bsu/rfact/java/laba6/MouseHandle.java
+++ src/bsu/rfact/java/laba6/MouseHandle.java
@@ -3,4 +3,5 @@ package bsu.rfact.java.laba6;
 import java.awt.event.MouseAdapter;
 
 public class MouseHandler extends MouseAdapter {
+
 }
